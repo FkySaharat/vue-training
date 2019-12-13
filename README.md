7994b6892791a7d4d7156094c7f5e8a7d4c63e3e
teebo00201 <teebo00201@gmail.com>
2019-12-13 11:36:44 +0700

    up file ...

--- README.md
+++ README.md
@@ -1,28 +1,24 @@
-# Vue Training
-- Create new working branch
-- Installation
-  - install vue cli
-  - initialize project with default settings
+# app-test
 
-## Task: Simple todo
-### Requirement
-- Display a list of todo items with its "Done" state (e.g. strikethrough text)
-- Sort newest item next to input
+## Project setup
+```
+npm install
+```
 
-input at the top | input at the bottom
---- | --- 
-\>input | `item1`
-`item3` | `item2`
-`item2` | `item3`
-`item1` | \>input
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
 
-- Add new todo with enter key
-- Checkbox for crossing out "Done" todo (toggleable)
-- Delete todo item
-- Clear button for removing all "Done" todo
-- Use localstorage as client database 
-  > https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API
+### Compiles and minifies for production
+```
+npm run build
+```
 
-### Optional Requirement
-- Edit todo text with double click
-- Dropdown Button menu for Edit and Delete per todo item
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/TodoList.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    
+    <header class="header">
+      <input type="text" class="todo-input" placeholder="What needs to be done">
+    </header>
+<section class="main" v-show="todos.length" v-cloak>
+      <input id="toggle-all" class="toggle-all" type="checkbox" v-model="allDone">
+      <label for="toggle-all"></label>
+      <ul class="todo-list">
+      <li v-for="todo in filteredTodos"
+        class="todo"
+        :key="todo.id"
+        :class="{ completed: todo.completed, editing: todo == editedTodo }">
+        <div class="view">
+          <input class="toggle" type="checkbox" v-model="todo.completed">
+          <label @dblclick="editTodo(todo)">{{ todo.title }}</label>
+          <button class="destroy" @click="removeTodo(todo)"></button>
+        </div>
+        <input class="edit" type="text"
+          v-model="todo.title"
+          v-todo-focus="todo == editedTodo"
+          @blur="doneEdit(todo)"
+          @keyup.enter="doneEdit(todo)"
+          @keyup.esc="cancelEdit(todo)">
+      </li>
+    </ul>
+</section>
+    <span class="todo-count">
+      <strong>{{ remaining }}</strong> {{ remaining | pluralize }} left
+    </span>
+
+    <ul class="filters">
+      <li><a href="#/all" :class="{ selected: visibility == 'all' }">All</a></li>
+      <li><a href="#/active" :class="{ selected: visibility == 'active' }">Active</a></li>
+      <li><a href="#/completed" :class="{ selected: visibility == 'completed' }">Completed</a></li>
+    </ul>
+  
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'todo-list',
+  data(){
+      return{
+           msg: 'Welcome to Your vue.js App'
+      }
+  }
+}
+</script>
+
+<style lang="scss">
+
+
+
+</style>
